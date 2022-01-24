914be2767fa60d092a7080d682e8b6df216f2b6e
anderson <andersonszdc@gmail.com>
2022-01-24 13:35:39 -0300

    set layout image

--- src/components/loginLayout/index.tsx
+++ src/components/loginLayout/index.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import Image from "next/image";
 import dog from "../../assets/dog.jpg";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  height: 100vh;
+
+  .wrapper__image {
+    position: relative;
+  }
+  
+  .image {
+    object-fit: cover;
+  }
+`
 
 const LoginLayout: React.FC = ({ children }) => {
   return (
-    <div>
+    <Wrapper>
       {children}
-      <div>
-        <Image alt="login-image" src={dog} />
+      <div className="wrapper__image">
+        <Image className="image" layout="fill" objectPosition="50% 100%" objectFit="cover" alt="login-image" src={dog} />
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
