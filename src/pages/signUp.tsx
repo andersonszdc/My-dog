d858634b853cbe4b6c64c9675b63600e59a0487f
anderson <andersonszdc@gmail.com>
2022-01-24 15:31:12 -0300

    created signUp page

---
+++ src/pages/signUp.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginLayout from "../components/loginLayout";
+
+const Index = () => {
+  return <div>oi</div>;
+};
+
+Index.layout = LoginLayout;
+
+export default Index;
