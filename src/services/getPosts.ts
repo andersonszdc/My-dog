f0f35b8e5c231f6e20667c818f06c050a9a198cc
anderson <andersonszdc@gmail.com>
2022-01-26 14:40:16 -0300

    sorted posts

--- src/services/getPosts.ts
+++ src/services/getPosts.ts
@@ -3,5 +3,9 @@ import { db } from "../utils/firebaseConfig";
 
 export const getPosts = async () => {
   const querySnapshot = await getDocs(collection(db, "posts"));
-  return querySnapshot.docs.map(doc => doc.data())
+  return querySnapshot.docs.map(doc => doc.data()).sort((a, b) => {
+    const numA = a.data.seconds
+    const numB = b.data.seconds
+    return numB - numA
+  })
 };
